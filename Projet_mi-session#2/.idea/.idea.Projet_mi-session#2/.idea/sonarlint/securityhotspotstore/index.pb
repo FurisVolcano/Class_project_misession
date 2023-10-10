cdf6e8e8ca22db9668ef8bdd2dc0570c97dbe0d4
FurisVolcano <128417417+FurisVolcano@users.noreply.github.com>
2023-10-10 10:52:12 -0400

    PlayerMovement(ScrollSider)

---
+++ Projet_mi-session#2/.idea/.idea.Projet_mi-session#2/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+P
+ Assets/Script/Camera_Movement.cs,7\5\7590d89c537c67b26209eea47422829764c1b14b
+P
+ Assets/Script/Player_Movement.cs,d\6\d6745649a34408e9392a68c3804366e05c9ab23c
--- Projet_mi-session#2/Assets/Script/Player_Movement.cs
+++ Projet_mi-session#2/Assets/Script/Player_Movement.cs
@@ -1,28 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player_Movement : MonoBehaviour
 {
-
-    private float x;
-    private float y;
+    public float speed;
+    public float jumpForce;
+    
+    
     Rigidbody2D rb2d;
-    [SerializeField]
-    private float speed;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isJumping = false;
 
-
-    private void Start()
+    private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+    }
+    
+    
+    private void Update()
+    {
+        InputProcess();
+        Animate();
     }
 
     private void FixedUpdate()
     {
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
-        rb2d.velocity =Time.deltaTime * speed * new Vector3(x,y,0).normalized;
+        Move();
+    }
 
+    private void Move()
+    {
+        rb2d.velocity = new Vector2(moveDirection * speed, rb2d.velocity.y);
+        if (isJumping)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+        }
 
+        isJumping = false;
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void InputProcess()
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal"); //scale of -1 -> 1
+        if (Input.GetButtonDown("Jump")) 
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
